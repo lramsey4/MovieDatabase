7ed6ee85106b412c16a70f5fd8a4b833defbb0f4
Lauren <lramsey4@elon.edu>
2024-12-04 01:58:19 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+/*
+ * Lauren Ramsey
+ * CSC3465 Software Design
+ * Robert Dutton and Shannon Duvall
+ * ExtraCredit MovieDatabase
+ * December 2-12th, 2024
+ */
+
+public class Main {
+	
+	public void main(String[] args) {
+		MovieApplication app = new MovieApplication();
+		app.initializeDatabase();
+		app.run();
+	}
+}
